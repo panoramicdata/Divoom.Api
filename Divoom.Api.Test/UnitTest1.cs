a131455a4ffcf93a27253acd046286cd70a7fafb
David Bond <david.bond@panoramicdata.com>
2022-11-21 22:50:43 +0000

    Add project files.

---
+++ Divoom.Api.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Divoom.Api.Test
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
---
+++ Divoom.Api/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Divoom.Api
+{
+	public class Class1
+	{
+
+	}
+}
